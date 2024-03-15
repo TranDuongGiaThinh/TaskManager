6eec82d1169b67d71f1f1bc84ba5efc9cc0c46f0
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-03-15 23:39:50 +0700

    edit colors, text

--- lib/info_app/my_colors.dart
+++ lib/info_app/my_colors.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-class MyColors{
+class MyColors {
   static const Color primary = Color.fromARGB(255, 255, 207, 147);
-  
 
-  static const Color now = Color.fromARGB(255, 3, 244, 244);
-  static const Color inProgress = Color.fromARGB(255, 255, 174, 0);
-  static const Color completed = Color.fromARGB(255, 0, 89, 255);
-  static const Color notYetDone = Colors.redAccent;
+  static const Color due = Colors.redAccent;
+  static const Color inProgress = Color.fromARGB(255, 3, 244, 244);
+  static const Color completed = Color.fromARGB(255, 0, 145, 255);
+  static const Color notYetDone = Color.fromARGB(255, 255, 174, 0);
 
   static const Color headerText = Colors.black87;
   static const Color normalText = Colors.black45;
@@ -18,4 +17,9 @@ class MyColors{
 
   static const Color premiumText = Colors.white;
   static const Color premiumBackground = Colors.blue;
+  
+  static const Color filterPannelItemBackground = Color.fromARGB(255, 234, 234, 234);
+  static const Color filterPannelItemText = Colors.black87;
+  static const Color filterPannelItemBackgroundSelected = Colors.orangeAccent;
+  static const Color filterPannelItemTextSelected = Colors.white;
 }
--- lib/info_app/my_constants.dart
+++ lib/info_app/my_constants.dart
@@ -2,21 +2,25 @@ class MyConstants{
   static const String appName = "Task Manager";
 
   static const String appFont = "roboto";
-  static const double largeFontSize = 12.0;
-  static const double mediumFontSize = 10.0;
-  static const double smallFontSize = 8.0;
+  static const double largeFontSize = 14.0;
+  static const double mediumFontSize = 12.0;
+  static const double smallFontSize = 10.0;
 
   static const double avatarSize = 50.0;
+  static const double chartSize = 93.0;
+  static const double noteSize = 15.0;
   
   static const String search = "Tìm kiếm...";
   static const String premium = "Premium";
   static const String statisticalWork = "Thống kê công việc";
-
   static const String completed = "Đã hoàn thành";
   static const String notYetDone = "Chưa thực hiện";
   static const String inProgress = "Đang thực hiện";
-  
+  static const String startTime = "Bắt đầu lúc";
   static const String deadlineTime = "Hết hạn lúc";
   static const String completionTime = "Hoàn thành lúc";
-  static const String startTime = "Bắt đầu lúc";
+  static const String due = "Đến hạn";
+  static const String expired = "Hết hạn";
+  static const String today = "Hôm nay";
+  static const String all = "Tất cả";
 }

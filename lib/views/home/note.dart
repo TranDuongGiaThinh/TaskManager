a49770b525e7609471865c226e7431442409f229
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-03-15 22:59:07 +0700

    Done note component

---
+++ lib/views/home/note.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/info_app/my_colors.dart';
+import 'package:task_manager/info_app/my_constants.dart';
+
+class Note extends StatelessWidget {
+  const Note(
+      {super.key,
+      required this.color,
+      required this.count,
+      required this.description});
+  final Color color;
+  final int count;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.all(5),
+          height: MyConstants.noteSize,
+          width: MyConstants.noteSize,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+        ),
+        Text(
+            "$count $description",
+            style: const TextStyle(
+                fontFamily: MyConstants.appFont,
+                fontSize: MyConstants.mediumFontSize,
+                color: MyColors.normalText),
+          ),
+      ],
+    );
+  }
+}

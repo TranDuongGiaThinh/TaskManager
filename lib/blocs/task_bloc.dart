5f628dac00c5438c3b9b2b71f52465145d8a9f27
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-03-22 19:16:29 +0700

    write function addTask, update, delete

--- lib/blocs/task_bloc.dart
+++ lib/blocs/task_bloc.dart
@@ -49,50 +49,34 @@ class FilterTasks extends TaskEvent {
 class TaskBloc extends Bloc<TaskEvent, TaskState> {
   TaskBloc() : super(const TaskState(allTasks: [], filteredTasks: [])) {
     on<LoadTasks>((event, emit) async {
-      List<Task> tasks = await TaskRepository().getListTask(UserPresenter.user.id);
+      List<Task> tasks =
+          await TaskRepository().getListTask(UserPresenter.user.id);
       emit(
         TaskState(allTasks: tasks, filteredTasks: tasks),
       );
     });
+
     on<AddTask>((event, emit) async {
-      final state = this.state;
-      List<Task> tasks = await List.from(state.allTasks)
-        ..add(event.task);
-      emit(
-        TaskState(
-          allTasks: tasks,
-          filteredTasks: tasks,
-        ),
-      );
+      TaskRepository().addTask(event.task);
+
+      add(LoadTasks(UserPresenter.user.id));
     });
+
     on<UpdateTask>((event, emit) async {
-      //
+      TaskRepository().updateTask(event.task);
+
+      add(LoadTasks(UserPresenter.user.id));
     });
+
     on<DeletedTask>((event, emit) async {
-      //
+      TaskRepository().deleteTask(event.task.id);
+
+      add(LoadTasks(UserPresenter.user.id));
     });
+    
     on<FilterTasks>((event, emit) async {
-      final state = this.state;
-      List<Task> filteredTasks;
-      switch (event.filterName) {
-        case MyConstants.due:
-          filteredTasks = state.allTasks.where((task) => task.isDue()).toList();
-          break;
-        case MyConstants.inProgress:
-          filteredTasks =
-              state.allTasks.where((task) => task.isInProgress()).toList();
-          break;
-        case MyConstants.expired:
-          filteredTasks =
-              state.allTasks.where((task) => task.isExpired()).toList();
-          break;
-        case MyConstants.completed:
-          filteredTasks =
-              state.allTasks.where((task) => task.isCompleted()).toList();
-          break;
-        default:
-          filteredTasks = state.allTasks;
-      }
+      List<Task> filteredTasks = TaskRepository().filterTasks(state.allTasks, event.filterName);
+
       emit(
         TaskState(
           allTasks: state.allTasks,

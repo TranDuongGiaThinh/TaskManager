7c255b86357708ba05a181ca88db1d2b03d5fe6b
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2024-03-25 15:31:00 +0700

    add dateTimePicker

---
+++ lib/views/task_detail/custom_date_time_picker.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:task_manager/utils/my_colors.dart';
+import 'package:task_manager/utils/my_constants.dart';
+import 'package:task_manager/utils/my_icon.dart';
+
+class CustomDateTimePicker extends StatefulWidget {
+  const CustomDateTimePicker(
+      {Key? key,
+      required this.type,
+      required this.datetime,
+      required this.onDateTimeChanged})
+      : super(key: key);
+
+  final String type;
+  final DateTime datetime;
+  final Function(DateTime) onDateTimeChanged;
+
+  @override
+  _CustomDateTimePickerState createState() => _CustomDateTimePickerState();
+}
+
+class _CustomDateTimePickerState extends State<CustomDateTimePicker> {
+  late DateTime selectedDateTime;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDateTime = widget.datetime;
+  }
+
+  Future<void> _selectDateTime(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: selectedDateTime,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (pickedDate != null) {
+      final TimeOfDay? pickedTime = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(selectedDateTime),
+      );
+      if (pickedTime != null) {
+        setState(() {
+          selectedDateTime = DateTime(
+            pickedDate.year,
+            pickedDate.month,
+            pickedDate.day,
+            pickedTime.hour,
+            pickedTime.minute,
+          );
+        });
+        widget.onDateTimeChanged(selectedDateTime);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _selectDateTime(context),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(MyIcon.watch,color: MyColors.normalText,),
+          SizedBox(width: 8),
+          Text(
+            '${widget.type} ${DateFormat('HH:mm dd/MM/yyyy').format(selectedDateTime)}',style: TextStyle(
+              fontFamily: MyConstants.appFont,
+              fontSize: MyConstants.mediumFontSize,
+              color: MyColors.normalText,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
